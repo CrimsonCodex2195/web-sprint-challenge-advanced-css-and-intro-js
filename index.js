04a02fb9225ee43d5c09c4e53ca9dd7ef55a5f6c
Shariq Ali <shariqali2195@gmail.com>
2021-04-30 20:06:42 -0400

    Completed task 4

--- index.js
+++ index.js
@@ -234,7 +234,6 @@ function getArtistByIndex(array, index) {
 }  
 
 
-
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Use get20s to do the following: 
 1. Receive an array
@@ -243,8 +242,16 @@ Use get20s to do the following:
 Example born in 1901 and died in 1959 - included -- born in 1889 and died in 1925 not included
 If correct, the function should return ["Salvador Dali", "Frida Kahlo"]*/
 
-function get20s(/*Your Code Here*/){
-  /*Your Code Here*/
+function get20s(array) {
+  let ninetiesArtists = [];
+  for (let i = 0; i < array.length; i ++) {
+    let splitArray = [];
+    splitArray = array[i].years.split(' - ');
+    if (splitArray[0].substring(0, 2) === '19' && splitArray[1].substring(0, 2) === '19') {
+      ninetiesArtists.push(array[i].name);
+    }
+  }
+  return ninetiesArtists;
 }
 
 
